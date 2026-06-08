76cb208467381f5ddf31d68fcce1a53a1687af90
JeronimoGarcia206 <jeronimogarcia206@gmail.com>
2026-06-08 14:54:59 -0300

    Se agrega el archivo principal

---
+++ TP Integrador Programación 1.py
@@ -0,0 +1,13 @@
+"""
+Trabajo Práctico Integrador 
+Programación 1  
+Gestión de Datos de Países en Python: filtros, ordenamientos y estadísticas
+"""
+
+#Funciones de Validación
+
+#Funciones 
+
+#Programa Principal
+
+print("TP")
