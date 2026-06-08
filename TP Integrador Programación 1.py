6cfe2ddf6870c072a977ec206c8bca744794bade
JeronimoGarcia206 <jeronimogarcia206@gmail.com>
2026-06-08 17:27:05 -0300

    Se agrego la funcion 1 del menú "Agregar país" y sus validaciones

--- TP Integrador Programación 1.py
+++ TP Integrador Programación 1.py
@@ -10,15 +10,24 @@ import csv
 def cargar_datos():
     """Carga los Datos desde el Archivo CSV"""
 
-    with open('Información Geografica.csv', mode='r', newline='', encoding='utf-8') as archivo:
-        lector = csv.DictReader(archivo)
+    paises = []
 
-        for fila in lector:
-            pais = {f"nombre": fila["nombre"],
-                    "poblacion": int(fila["poblacion"]),
-                    "superficie": int(fila["superficie"]),
-                    "continente": fila["continente"]}
-            paises.append(pais)
+    try:
+        with open('Información Geografica.csv', mode='r', newline='', encoding='utf-8') as archivo:
+            lector = csv.DictReader(archivo)
+
+            for fila in lector:
+                pais = {f"nombre": fila["nombre"],
+                        "poblacion": int(fila["poblacion"]),
+                        "superficie": int(fila["superficie"]),
+                        "continente": fila["continente"]}
+                paises.append(pais)
+
+    except FileNotFoundError:
+            print("Error: No se encontró el archivo.")
+
+    except Exception as e:
+            print(f"Error inesperado: {e}")
 
     return paises
 
@@ -28,17 +37,118 @@ def mostrar_paises(paises):
     for pais in paises:
         print(pais)
 
+def buscar_pais(paises, nombre):
+    """Esta Función Busca Países en la Lista"""
+    for pais in paises:
+        if pais["nombre"] == nombre:
+            return pais
+
+    return None
 
 #Funciones de Validación
-def validar_opcion(opcion):
-    """Valida que la Opción del Menú"""
+def validar_opcion():
+    """Solicita y valida la opción del menú"""
 
-    while opcion not in ["1", "2", "3", "4", "5", "6", "7"]:
-        print("Error: Opción inválida.")
-        opcion = input("Seleccione una opción: ")
+    while True:
+        try:
+            opcion = input("Seleccione una opción: ")
 
-    return opcion
+            if opcion not in ["1", "2", "3", "4", "5", "6", "7"]:
+                raise ValueError("Debe ingresar una opción entre 1 y 7")
 
+            return opcion
+
+        except ValueError as e:
+            print(f"Error de ingreso: {e}")
+
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+
+def validar_pais():
+    """Solicita y valida el nombre del país"""
+
+    while True:
+        try:
+            nombre = input("Ingrese el nombre del país: ").strip().title()
+
+            if nombre == "":
+                raise ValueError("El nombre no puede estar vacío")
+
+            if not nombre.replace(" ", "").isalpha():
+                raise ValueError("El nombre solo puede contener letras")
+
+            if buscar_pais(paises, nombre) is not None:
+                raise ValueError("El país ya existe")
+
+            return nombre
+
+        except ValueError as e:
+            print(f"Error de ingreso: {e}")
+
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+
+def validar_poblacion():
+    """Solicita y valida la población"""
+
+    while True:
+        try:
+            poblacion = int(input("Ingrese la cantidad de población: "))
+
+            if poblacion <= 0:
+                raise ValueError("Debe ingresar un número mayor que cero")
+
+            return poblacion
+
+        except ValueError as e:
+            print(f"Error de ingreso: {e}")
+
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+
+def validar_superficie():
+    """Solicita y valida la superficie"""
+
+    while True:
+        try:
+            superficie = int(input("Ingrese la cantidad de superficie: "))
+
+            if superficie <= 0:
+                raise ValueError("Debe ingresar un número mayor que cero")
+
+            return superficie
+
+        except ValueError as e:
+            print(f"Error de ingreso: {e}")
+
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+
+def validar_continente():
+    """Solicita y valida el continente"""
+
+    continentes = ["America", "Europa", "Asia", "Oceania", "Africa"]
+
+    while True:
+        try:
+            continente = input("Ingrese el continente: ").strip().title()
+
+            if continente == "":
+                raise ValueError("El continente no puede estar vacío")
+
+            if not continente.replace(" ", "").isalpha():
+                raise ValueError("El continente solo puede contener letras")
+
+            if continente not in continentes:
+                raise ValueError("Continente inválido")
+
+            return continente
+
+        except ValueError as e:
+            print(f"Error de ingreso: {e}")
+
+        except Exception as e:
+            print(f"Error inesperado: {e}")
 
 #Funciones del Menú
 def muestra_menu():
@@ -52,23 +162,39 @@ def muestra_menu():
     print("6. Mostrar estadísticas")
     print("7. Salir")
 
-def pedir_opcion():
-    """Solicita una Opción del Menú"""
+def agregar_pais():
+    """Esta Función Agrega un País"""
 
-    opcion = input("Seleccione una opción: ")
-    return opcion
+    nombre = validar_pais()
+    poblacion = validar_poblacion()
+    superficie = validar_superficie()
+    continente = validar_continente()
+
+    pais = {
+        "nombre": nombre,
+        "poblacion": poblacion,
+        "superficie": superficie,
+        "continente": continente
+    }
+
+    paises.append(pais)
+
+    print("País agregado correctamente.")
 
 
 #Programa Principal
-paises = [] #Lista con el Contenido
+paises = cargar_datos()  #Lista con el Contenido
 
 
 while True: #Menú Interactivo
     muestra_menu()
-    opcion = pedir_opcion()
-    opcion = validar_opcion(opcion)
+    opcion = validar_opcion()
 
+    if opcion == "1":
+        agregar_pais()
 
-    if opcion == "7":
+    elif opcion == "7":
         print("Programa Cerrado")
         break
+
+mostrar_paises(paises)
